b71ad4c3b7715eab78bddec5059554a476d8d645
esamjr <agildimasjr@gmail.com>
2020-09-02 16:31:35 +0700

    added

---
+++ application/views/welcome_message.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Portfolio</title>
+		<link rel="icon" type="image/x-icon" href="<?php echo base_url('assets/img/favicon.ico') ?>" />
+		<!-- Dev icons -->
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@master/devicon.min.css">
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.13.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="<?php echo base_url('css/styles.css') ?>" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">
+                <span class="d-block d-lg-none">Clarence Taylor</span>
+                <span class="d-none d-lg-block"><img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="<?php echo base_url('assets/img/profile1.png') ?>" alt="" /></span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">About</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#experience">Experience</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#education">Education</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#skills">Skills</a></li>
+                    <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#interests">Interests</a></li>
+                </ul>
+            </div>
+        </nav>
+        <!-- Page Content-->
+        <div class="container-fluid p-0">
+            <!-- About-->
+            <section class="resume-section" id="about">
+                <div class="resume-section-content">
+                    <h1 class="mb-0">
+                        Agil
+                        <span class="text-primary">Dimas</span>
+                    </h1>
+                    <div class="subheading mb-5">
+                        Backend Developer · Student · Gamer · +62 8812-512-990 ·
+                        <a href="mailto:name@email.com">agildimasjr@gmail.com</a>
+                    </div>
+                    <p class="lead mb-5">My Name Agil Dimas Hartanto Im from Jakarta I live In Grogol Petamburan Jakarta Barat , I was Born in 1 january 2003, I like Something About digital thing and technology.  Nowadays Im student of 2 Vocational Highschool Gambir, And Active in Inatec (Information Technology).</p>
+                    <div class="social-icons">
+                        <a class="social-icon" href="https://www.linkedin.com/in/agil-dimas-53a8711a6/"><i class="fab fa-linkedin-in"></i></a>
+                        <a class="social-icon" href="https://github.com/esamjr/"><i class="fab fa-github"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-twitter"></i></a>
+                        <a class="social-icon" href="#"><i class="fab fa-facebook-f"></i></a>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Experience-->
+            <section class="resume-section" id="experience">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Experience</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">Internship as Backend Developer</h3>
+                            <div class="subheading mb-3">PT.Solusindo Pratama</div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ornare arcu odio ut sem nulla pharetra diam. Et malesuada fames ac turpis egestas integer eget aliquet nibh. Enim praesent elementum facilisis leo vel fringilla est ullamcorper eget. Viverra ipsum nunc aliquet bibendum enim facilisis gravida. Gravida in fermentum et sollicitudin. Mauris pellentesque pulvinar pellentesque habitant. Proin sagittis nisl rhoncus mattis rhoncus urna neque. Sit amet facilisis magna etiam tempor orci eu lobortis. Dignissim convallis aenean et tortor at. Diam maecenas sed enim ut sem viverra. Iaculis at erat pellentesque adipiscing commodo. Quis enim lobortis scelerisque fermentum. Non blandit massa enim nec dui nunc mattis enim.</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">Febuary 2020 - Mei 2020</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Education-->
+            <section class="resume-section" id="education">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Education</h2>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMKN 2 Jakarta</h3>
+                            <div class="subheading mb-3">Software Engineer</div>
+                            <p>GPA: 3.50/4.00</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2018 - Present</span></div>
+                    </div>
+                    <div class="d-flex flex-column flex-md-row justify-content-between mb-5">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SMPN 82 Jakarta</h3>
+                            <div class="subheading mb-3">Academic Student</div>
+                            <p>GPA: 2.60/4.00</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2015 - 2018</span></div>
+					</div>
+					<div class="d-flex flex-column flex-md-row justify-content-between">
+                        <div class="flex-grow-1">
+                            <h3 class="mb-0">SDN 07 Jakarta</h3>
+                            <div class="subheading mb-3">Academic Student</div>
+                            <p>GPA: 2.10/3.00</p>
+                        </div>
+                        <div class="flex-shrink-0"><span class="text-primary">2009 - 2015</span></div>
+                    </div>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Skills-->
+            <section class="resume-section" id="skills">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Skills</h2>
+                    <div class="subheading mb-3">Programming Languages & Tools</div>
+                    <ul class="list-inline dev-icons">
+						<li class="list-inline-item"><i class="fab fa-html5"></i></li>
+						<li class="list-inline-item"><i class="fab fa-bootstrap"></i></li>
+                        <li class="list-inline-item"><i class="fab fa-python"></i></li>
+						<li class="list-inline-item"><i class="devicon-django-line"></i></li>
+						<li class="list-inline-item"><i class="devicon-go-plain"></i></li>
+						<li class="list-inline-item"><i class="devicon-postgresql-plain-wordmark"></i></li>
+						<li class="list-inline-item"><i class="devicon-mysql-plain-wordmark"></i></li>
+						<li class="list-inline-item"><i class="fab fa-git"></i></li>
+						<li class="list-inline-item"><i class="devicon-redis-plain-wordmark"></i></li>
+                    </ul>
+                    <div class="subheading mb-3">Interest to learn :</div>
+                    <ul class="fa-ul mb-0">
+                        <li>
+                            <span class="fa-li"><i class="devicon-docker-plain-wordmark"></i></span>
+                            Docker
+						</li>
+						<li>
+                            <span class="fa-li"><i class="devicon-nodejs-plain-wordmark"></i></span>
+                            NodeJS
+                        </li>
+                        <li>
+                            <span class="fa-li"><i class="devicon-vuejs-plain"></i></span>
+                            VueJS
+                        </li>
+                        
+                        <li>
+                            <span class="fa-li"><i class="devicon-react-original-wordmark"></i></span>
+                            ReactJS
+                        </li>
+                    </ul>
+                </div>
+            </section>
+            <hr class="m-0" />
+            <!-- Interests-->
+            <section class="resume-section" id="interests">
+                <div class="resume-section-content">
+                    <h2 class="mb-5">Dreams</h2>
+                    <p>Being a developer of big company or own a company in technology.</p>
+					<p class="mb-0">I hope i have good career in the future.</p>
+					<p class="mb-0">Got a scholarship to University.</p>
+                </div>
+            </section>
+            <hr class="m-0" />
+        </div>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="<?php echo base_url('js/scripts.js') ?>"></script>
+    </body>
+</html>
